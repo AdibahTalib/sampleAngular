630e2caa7437db48ccbcb98d5994d2944c25d54f
unknown <dibatalibjali@gmail.com>
2021-03-25 16:54:03 +0800

    updates

---
+++ my-covid-app/src/app/mining/mining.component.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mining',
+  templateUrl: './mining.component.html',
+  styleUrls: ['./mining.component.css']
+})
+export class MiningComponent implements OnInit {
+
+  constructor(
+    // Inject your HTTP Client Service here
+    private httpClient: HttpClient,
+    ) {
+    
+  }
+
+  ngOnInit(): void {
+    // this.getCovid();
+
+  }
+
+  mining: string = "Covid Mining Services";
+
+  input: string = "";
+
+  public getBasicMining(): any {
+    this.httpClient.get(`http://localhost:8081/covid/mining/my`, { responseType: 'text' })
+      .subscribe((data: any) => 
+                  {
+                    // assign HTTP response with local variable
+                    this.input = data;
+                  }
+                );   
+  }
+}
